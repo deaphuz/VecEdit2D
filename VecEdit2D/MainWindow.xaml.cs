3c7f5030b70fe638db0e69f82fe3021954a11e5f
Chramar2 <konrado5376@gmail.com>
2024-01-21 23:46:00 +0100

    added Command design pattern

--- VecEdit2D/MainWindow.xaml.cs
+++ VecEdit2D/MainWindow.xaml.cs
@@ -41,7 +41,6 @@ namespace VecEdit2D
             appStateInstance = AppState.Instance;
             refImage = Image.Instance;
             points = new List<Point>();
-
             appStateInstance.refSelectedShapeGroup = refImage.canvas;
         }
 
@@ -239,8 +238,8 @@ namespace VecEdit2D
             MainCanvas.Children.Clear();
 
             //make translation
-            appStateInstance.refSelectedShapeGroup.translate(toolboxInstance.trX, toolboxInstance.trY);
-
+            Command translateCommand = new TranslateCommand(appStateInstance.refSelectedShapeGroup, toolboxInstance.trX, toolboxInstance.trY);
+            translateCommand.execute();
             //redraw image
             RedrawImage();
 
@@ -252,8 +251,8 @@ namespace VecEdit2D
             MainCanvas.Children.Clear();
 
             //make rotation
-            appStateInstance.refSelectedShapeGroup.rotate(toolboxInstance.rotAngle * (Math.PI / 180.0), appStateInstance.refSelectedShapeGroup.center);
-
+            Command rotateCommand = new RotateCommand(appStateInstance.refSelectedShapeGroup, toolboxInstance.rotAngle);
+            rotateCommand.execute();
             //redraw image
             RedrawImage();
         }
@@ -264,7 +263,8 @@ namespace VecEdit2D
             MainCanvas.Children.Clear();
 
             //scale
-            appStateInstance.refSelectedShapeGroup.scale(toolboxInstance.scaleFactor, toolboxInstance.scaleFactor, appStateInstance.refSelectedShapeGroup.center);
+            Command scaleCommand = new ScaleCommand(appStateInstance.refSelectedShapeGroup, toolboxInstance.scaleFactor);
+            scaleCommand.execute();
 
             //redraw image
             RedrawImage();
